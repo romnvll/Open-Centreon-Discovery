f75e0331abb7453d51f33dd13a8e297665ed49cf
romnvll <vallee.romain@gmail.com>
2020-12-03 20:13:32 +0100

    remove remember me

--- login.php
+++ login.php
@@ -19,9 +19,9 @@
       <label for="inputPassword" class="sr-only">Password</label>
       <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Centreon Password" >
       <div class="checkbox mb-3">
-        <label>
+        <!-- <label>
           <input type="checkbox" value="remember-me"> Remember me
-        </label>
+        </label>-->
       </div>
       <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       <p class="mt-5 mb-3 text-muted">Open Centreon Host Discovery</p>
