395fdfb514a79973b66ed086521929b6fb244b53
romnvll <vallee.romain@gmail.com>
2021-02-04 16:16:42 +0100

    Update config.php

--- config.php
+++ config.php
@@ -7,13 +7,10 @@ $config['centreon']['password'] = 'admin';
 //chemin de clapi
 $config['centreon']['clapi'] = '/usr/share/centreon/bin/centreon';
 
-//Le nom de votre poller
-//centreon -u admin -p password -a POLLERLIST
-$config['pollers']['poller1']='Central';
 
 //pour le scan BackGround
 //activation du scan backGround pour les /16 : true or false
-$config['backGroundScan']['use'] = true;
+$config['backGroundScan']['use'] = false;
 
 $config['backGroundScan'][0] = array("network" => "192.168.4.0/24","community"=>"public","version"=>"2c" );
 $config['backGroundScan'][1] = array("network" => "10.3.0.0/16","community"=>"public","version"=>"2c" );
