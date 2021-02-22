92414b23b219a09dc3903d9ac17c3b21a722b574
romnvll <vallee.romain@gmail.com>
2021-02-22 19:30:28 +0100

    Update config.php

--- config.php
+++ config.php
@@ -13,7 +13,7 @@ $config['centreon']['clapi'] = '/usr/share/centreon/bin/centreon';
 
 
 //activation du scan backGround pour les /16 et plus : true or false
-$config['backGroundScanUse'] = true;
+$config['backGroundScanUse'] = false;
 //chemin de l'executable scanBackGround
 
 $config['backGroundRoot'] = '/usr/share/discovery';
