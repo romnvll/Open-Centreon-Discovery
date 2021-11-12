44bc1a59839c4bb607ea7ac8a52d2b52f193c2b0
Romnvll <vallee.romain@gmail.com>
2021-11-12 21:52:26 +0100

    fix bugs

--- add.php
+++ add.php
@@ -1,9 +1,10 @@
 <?php
+/*
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-session_start();
-if (!$_SESSION['isAdmin']) {
+*/
+if (!$_COOKIE['isAdmin']) {
 
 	header('Location: login.php');
 	exit();
